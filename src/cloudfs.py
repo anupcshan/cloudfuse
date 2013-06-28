8c46daeb9286141038858573e30cd8e02f7990ae
Anup Chenthamarakshan <anupcshan@gmail.com>
2013-06-27 22:58:59 -0800

    Dummy statfs implementation.

--- src/cloudfs.py
+++ src/cloudfs.py
@@ -4,6 +4,23 @@ import llfuse
 import sys
 
 class CloudFSOperations(llfuse.Operations):
+    def statfs(self):
+        stat_ = llfuse.StatvfsData()
+
+        stat_.f_bsize = 512
+        stat_.f_frsize = 512
+
+        size = 10000000000
+        stat_.f_blocks = size // stat_.f_frsize
+        stat_.f_bfree = max(size // stat_.f_frsize, 1024)
+        stat_.f_bavail = stat_.f_bfree
+
+        inodes = 10000
+        stat_.f_files = inodes
+        stat_.f_ffree = max(inodes , 100)
+        stat_.f_favail = stat_.f_ffree
+
+        return stat_
     pass
 
 if __name__ == '__main__':
