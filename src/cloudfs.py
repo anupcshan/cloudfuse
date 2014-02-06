20203ffde1ba938b1d22e61dd805f6753444d78e
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-02-05 23:13:26 -0800

    Skeleton to mount and list files and folders. As of now, it still does nothing. + Shamelessly taken from https://github.com/anupcshan/mycloud.

--- src/cloudfs.py
+++ src/cloudfs.py
@@ -2,20 +2,51 @@
 """CloudFS FUSE filesystem."""
 
 import argparse
+import errno
 import llfuse
 import logging
+import os
+import stat
 from endpoint import EndPoint
+from time import time
 
 # pylint: disable-msg=W0611
 from dropbox_endpoint import DropBoxEndPoint
 from copy_endpoint import CopyEndPoint
 # pylint: enable-msg=W0611
 
+class Inode:
+    __current_id = 0
+    inodes = {}
+
+    def __init__(self):
+        self.id = Inode.get_next_id()
+        self.name = ''
+        self.isDir = True
+        self.size = 0
+        self.permissions = None
+        self.uid = os.getuid()
+        self.gid = os.getgid()
+        self.atime = self.ctime = self.mtime = time()
+        self.children = []
+        self.parent = None
+        Inode.inodes[self.id] = self
+
+    @classmethod
+    def get_next_id(cls):
+        Inode.__current_id += 1
+        return Inode.__current_id
+
+ROOT_INODE = Inode()
+ROOT_INODE.parent = ROOT_INODE
+ROOT_INODE.permissions = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IFDIR | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+
 class CloudFSOperations(llfuse.Operations):
     """CloudFS implementation of llfuse Operations class."""
     def __init__(self):
         super(CloudFSOperations, self).__init__()
         EndPoint.load_saved_endpoints()
+        self.tree = ROOT_INODE
 
     def statfs(self):
         stat_ = llfuse.StatvfsData()
@@ -39,6 +70,69 @@ class CloudFSOperations(llfuse.Operations):
         stat_.f_favail = stat_.f_ffree = stat_.f_files = 10000
 
         return stat_
+
+    def lookup(self, parent_inode, name):
+        inode = None
+        if name == '.':
+            inode = parent_inode
+        elif name == '..':
+            inode = Inode.inodes[parent_inode].parent.id
+        else:
+            parent = Inode.inodes[parent_inode].parent
+            for child in parent.children:
+                if child.name == name:
+                    inode = child.id
+
+        if inode:
+            return self.getattr(inode)
+
+        raise(llfuse.FUSEError(errno.ENOENT))
+
+    def opendir(self, inode):
+        return inode
+
+    def readdir(self, inode, off):
+        print 'Readdir of inode %d at offset %d' % (inode, off)
+        node = Inode.inodes[inode]
+        
+        i = off
+        for child in node.children[off:]:
+            if child.name.count('/') == 0:
+                i += 1
+                yield (child.name.replace('/', '//'), self.getattr(child.id), i)
+
+    def getattr(self, inode):
+        node = Inode.inodes[inode]
+        print 'Calling getattr on inode %d : %s' % (inode, node.name)
+
+        entry = llfuse.EntryAttributes()
+        entry.st_ino = inode
+        entry.generation = 0
+        entry.entry_timeout = 300
+        entry.attr_timeout = 300
+        entry.st_mode = node.permissions
+        entry.st_nlink = len(node.children) + 1
+        entry.st_uid = node.uid
+        entry.st_gid = node.gid
+        entry.st_rdev = 0
+        entry.st_size = node.size
+
+        entry.st_blksize = 512
+        entry.st_blocks = 1
+        entry.st_atime = node.atime
+        entry.st_mtime = node.mtime
+        entry.st_ctime = node.ctime
+
+        return entry
+
+    def open(self, inode, flags):
+        print 'Opening file %d with flags %s' % (inode, flags)
+
+        self.inode_open_count[inode] += 1
+        return inode
+
+    def access(self, inode, mode, ctx):
+        return True
 
     def auto_create_filesystem(self):
         """
